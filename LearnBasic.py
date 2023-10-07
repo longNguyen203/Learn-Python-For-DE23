1e27fb782be6c0e0b0bfe9f3b332b37f692bac63
LongDE2003 <longlovers2003@gmail.com>
2023-10-07 21:15:18 +0700

    add new file

---
+++ LearnBasic.py
@@ -0,0 +1,4 @@
+# lambda function
+lst = [1,2,3,4,5]
+lst_new = list(map(lambda x: x * x, lst))
+print(lst_new)
---
+++ calc.py
@@ -0,0 +1,48 @@
+
+def add(x, y):
+    """Add Function"""
+    return x + y
+
+def subtract(x, y):
+    """Subtract Function"""
+    return x - y
+
+def multiply(x, y):
+    """Multiply Function"""
+    return x * y
+
+def divide(x, y):
+    """Divide Function"""
+    if y == 0:
+        raise ValueError("khong the chia cho 0")
+    return x / y
+
+
+print(add(1, 5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
